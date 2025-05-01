03009557cf68ef0901a294dd372f669db72c50c7
tommysoeder <tommysoeder@Soeder97.>
2025-05-01 12:50:11 +0200

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -11,5 +11,28 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	unsigned char	*ptr;
 
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	char buffer[20] = "Hello !";
+	printf("Antes de ft_bzero: %s\n", buffer);
+
+	ft_bzero(buffer, 3);
+
+	printf("Después de ft_bzero: %s\n", buffer);	
+}*/
