cd4aa0610e46b1db6f3403a475c7cf146d7ee0c1
tommysoeder <tommysoeder@Soeder97.>
2025-05-13 11:30:25 +0200

    ft_itoa SIN HEADER

--- Makefile
+++ Makefile
@@ -29,6 +29,7 @@ ft_substr.c \
 ft_strjoin.c \
 ft_strtrim.c \
 ft_split.c \
+ft_itoa \
 
 OBJ = $(SRC:.c=.o)
 RM = rm -f
---
+++ ft_itoa.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+// #include <stdio.h>
+
+static int	ft_intlen(int n)
+{
+	int	len;
+
+	len = (n <= 0);
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	long	nb;
+
+	nb = n;
+	len = ft_intlen(nb);
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	str[len--] = '\0';
+	if (nb == 0)
+		str[0] = '0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (nb)
+	{
+		str[len--] = '0' + (nb % 10);
+		nb /= 10;
+	}
+	return (str);
+}
+/*
+int main(void)
+{
+    printf("itoa(42) = %s\n", ft_itoa(42));
+	printf("itoa(0) = %s\n", ft_itoa(0));
+	printf("itoa(-1234) = %s\n", ft_itoa(-1234));
+	printf("itoa(-2147483648) = %s\n", ft_itoa(-2147483648));
+}*/
